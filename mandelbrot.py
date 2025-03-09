30a387a0b0fa47d14e663869906332d8ae2a471d
aseelismail10 <112965434+aseelismail10@users.noreply.github.com>
2025-03-09 17:35:48 -0400

    Update mandelbrot.py
    Added the PART 1 to the repo

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,5 +1,27 @@
 import numpy as np
 
+"""PART 1"""
+def get_escape_time(c: complex, max_iterations: int) -> int | None:
+    """finding the max amount of times c has to go through the mandelbrot set before it escapes"""
+    z = c  # Start at c (as given in assignment)
+    if abs(z) > 2:
+        return 0  # Escape immediately if magnitude exceeds 2
+
+    for i in range(1, max_iterations + 1):  # Start count from 1 to match expected output
+        z = z ** 2 + c  # Mandelbrot iteration
+        if abs(z) > 2:
+            return i  # Return number of iterations until escape
+
+    return None  # Did not escape within max_iterations
+"""testing code"""
+print(get_escape_time(2+1j, 5))
+print(get_escape_time(1+1j, 10))
+print(get_escape_time(0.5+0.5j, 3))
+print(get_escape_time(0.5+0.5j, 4))
+print(get_escape_time(0.38+0.25j, 100))
+
+"""PART 2"""
+
 def get_complex_grid(top_left: complex, bottom_right: complex, step: float) -> np.ndarray:
     """
 

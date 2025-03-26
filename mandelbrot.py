68bd675f43e9b13c9404818e4b8fb829d861836d
婧兮Flora <yunyang.zhong@ufl.edu>
2025-03-26 15:10:08 +0800

    Using a more numpy way to do Part 3 and Part 4.

--- mandelbrot.py
+++ mandelbrot.py
@@ -81,21 +81,15 @@ def get_escape_time_color_arr(c_arr: np.ndarray,max_iterations: int) -> np.ndarr
     np.ndarray, An array of the same shape with color values in [0, 1] according to the escape time of each c-value.
 
     """
-    escape_time = np.zeros(c_arr.shape, dtype = int)
-    current_value = np.zeros(c_arr.shape, dtype = complex)
-    num_iterations = np.zeros(c_arr.shape, dtype = int)
-    for i in range(c_arr.shape[0]):
-        for j in range(c_arr.shape[1]):
-            c = c_arr[i, j]
-            current_value[i,j] = 0 + 0j
-            num_iterations[i,j] = 0
-            while np.abs(current_value[i,j]) <= 2 and num_iterations[i,j] < max_iterations:
-                current_value[i,j] = current_value[i,j] * current_value[i,j] + c
-                num_iterations[i,j] += 1
-            if num_iterations[i,j] == max_iterations:
-                escape_time[i, j] = max_iterations + 1
-            else:
-                escape_time[i, j] = num_iterations[i,j]
+    z_val = np.zeros(c_arr.shape, dtype=complex)
+    escape_time = np.full(c_arr.shape, max_iterations, dtype=int)
+    previously_not_escape = np.ones(c_arr.shape, dtype=bool)
+    for i in range(max_iterations):
+        z_val[previously_not_escape] = z_val[previously_not_escape] ** 2 + c_arr[previously_not_escape]
+        escaped = np.abs(z_val) > 2
+        now_escaping = (escaped == True) & (previously_not_escape == True)
+        escape_time[now_escaping] = i # Record that the point is first escaped at the i iteration.
+        previously_not_escape[now_escaping] = False
     result = (max_iterations - escape_time + 1) / (max_iterations + 1)
     return result
 
@@ -117,19 +111,15 @@ def get_julia_color_arr(grid: np.ndarray, c: complex, max_iterations: int) -> np
         np.ndarray: A 2D array of escape times.
 
     """
-    z = grid.copy()
-    escape_times = np.zeros(grid.shape, dtype=int)  # Default to max iterations
+    z_val = grid.copy()
+    escape_time = np.full(grid.shape, max_iterations, dtype=int)
+    previously_not_escape = np.ones(grid.shape, dtype=bool)
     escape_radius = np.maximum(abs(c), 2)
-    for i in range(grid.shape[0]):
-        for j in range(grid.shape[1]):
-            current_value = z[i, j]
-            num_iterations = 0
-            while num_iterations < max_iterations and np.abs(current_value) < escape_radius:
-                current_value = current_value ** 2 + c
-                num_iterations += 1
-                if num_iterations < max_iterations:
-                    escape_times[i, j] = num_iterations
-                else:
-                    escape_times[i, j] = max_iterations + 1
-    result = (max_iterations - escape_times + 1) / (max_iterations + 1)
+    for i in range(max_iterations):
+        z_val[previously_not_escape] = z_val[previously_not_escape] ** 2 + c
+        escaped = np.abs(z_val) > escape_radius
+        now_escaping = (escaped == True) & (previously_not_escape == True)
+        escape_time[now_escaping] = i
+        previously_not_escape[now_escaping] = False
+    result = (max_iterations - escape_time + 1) / (max_iterations + 1)
     return result
